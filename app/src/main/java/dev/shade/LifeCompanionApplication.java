40a97c3e65cf2fde8e20b1324e190cb03d6c3824
Shade <shade@shade.ga>
2023-09-03 11:48:32 +0400

    Initial Commit

--- app/src/main/java/dev/shade/LifeCompanionApplication.java
+++ app/src/main/java/dev/shade/LifeCompanionApplication.java
@@ -2,12 +2,9 @@ package dev.shade;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
-public class LifeCompanionApplication
-{
+public class LifeCompanionApplication {
     public static void main(String[] args) {
         SpringApplication.run(LifeCompanionApplication.class, args);
     }
