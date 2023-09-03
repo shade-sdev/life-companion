d8d66079b318a201c44b67539e0fe2303a2b67ba
Shade <shade@shade.ga>
2023-09-03 17:14:55 +0400

    Initial Commit

--- user/src/main/java/dev/shade/infrastructure/configuration/UserModuleConfig.java
+++ user/src/main/java/dev/shade/infrastructure/configuration/UserModuleConfig.java
@@ -1,6 +1,7 @@
 package dev.shade.infrastructure.configuration;
 
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -13,5 +14,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan("dev.shade.infrastructure.repository")
 @EnableJpaAuditing
 @EnableTransactionManagement
+@EnableCaching
 public class UserModuleConfig {
 }
--- user/src/main/java/dev/shade/infrastructure/repository/user/impl/JpaUserRepositoryImpl.java
+++ user/src/main/java/dev/shade/infrastructure/repository/user/impl/JpaUserRepositoryImpl.java
@@ -5,6 +5,9 @@ import dev.shade.domain.user.User;
 import dev.shade.infrastructure.repository.user.JpaUserMapper;
 import dev.shade.infrastructure.repository.user.UserJpaEntityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -23,6 +26,10 @@ public class JpaUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Caching(evict = {
+            @CacheEvict(value = "users", key = "#user.id"),
+            @CacheEvict(value = "users", key = "#user.userName"),
+    })
     public User save(User user) {
         return Optional.of(user)
                        .map(mapper::mapToEntity)
@@ -32,12 +39,14 @@ public class JpaUserRepositoryImpl implements UserRepository {
     }
 
     @Override
+    @Cacheable(value = "users", key = "#id")
     public Optional<User> findById(UUID id) {
         return repository.findById(id)
                          .map(mapper::mapToUser);
     }
 
     @Override
+    @Cacheable(value = "users", key = "#username")
     public Optional<User> findBy(String username) {
         return repository.findByUserName(username)
                          .map(mapper::mapToUser);
