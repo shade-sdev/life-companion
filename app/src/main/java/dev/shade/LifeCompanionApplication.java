ced43d8815a88f40599ec14a99400f646419f3b4
Shade <Shade@Shade.ga>
2024-03-09 09:43:04 +0400

    openshift

--- app/src/main/java/dev/shade/LifeCompanionApplication.java
+++ app/src/main/java/dev/shade/LifeCompanionApplication.java
@@ -5,8 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LifeCompanionApplication {
-    public static void main(String[] args) throws InterruptedException {
-        Thread.sleep(200000);
+    public static void main(String[] args) {
         SpringApplication.run(LifeCompanionApplication.class, args);
     }
 }
